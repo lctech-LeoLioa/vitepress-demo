c72bc2ddf4b7b3cde757c4bf5b7b91f77cd36d79
Leo Liao <leo@LeodeMacBook-Pro.local>
2024-09-20 14:56:05 +0800

    chore: remove useless config

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ const env = loadEnv(process.env.MODE!, process.cwd())
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    esbuild: {
-      drop: mode === 'production' ? ['console', 'debugger'] : [],
-    },
     define: {
       'process.env': env,
     },
